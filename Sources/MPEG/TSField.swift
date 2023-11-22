10a6ef939dc963cfc270b2ffb4dec49309f7ae4c
Erik Moqvist <erik.moqvist@gmail.com>
2023-11-22 20:15:28 +0100

    Refactoring.

--- Sources/MPEG/TSField.swift
+++ Sources/MPEG/TSField.swift
@@ -5,10 +5,8 @@ class TSAdaptationField {
 
     var length: UInt8 = 0
     var randomAccessIndicator = false
-    var splicingPointFlag = false
     var pcr: Data?
     var spliceCountdown: UInt8 = 0
-    var adaptationExtension: TSAdaptationExtensionField?
     var stuffingBytes: Data?
 
     init() {}
@@ -21,9 +19,6 @@ class TSAdaptationField {
         length = UInt8(truncatingIfNeeded: TSAdaptationField.fixedSectionSize)
         if let pcr {
             length += UInt8(truncatingIfNeeded: pcr.count)
-        }
-        if let adaptationExtension {
-            length += adaptationExtension.length + 1
         }
         if let stuffingBytes {
             length += UInt8(truncatingIfNeeded: stuffingBytes.count)
@@ -43,15 +38,11 @@ extension TSAdaptationField: DataConvertible {
             var byte: UInt8 = 0
             byte |= randomAccessIndicator ? 0x40 : 0
             byte |= pcr != nil ? 0x10 : 0
-            byte |= splicingPointFlag ? 0x04 : 0
             let buffer = ByteArray()
                 .writeUInt8(length)
                 .writeUInt8(byte)
             if let pcr {
                 buffer.writeBytes(pcr)
-            }
-            if splicingPointFlag {
-                buffer.writeUInt8(spliceCountdown)
             }
             if let stuffingBytes {
                 buffer.writeBytes(stuffingBytes)
@@ -62,12 +53,8 @@ extension TSAdaptationField: DataConvertible {
             let buffer = ByteArray(data: newValue)
             do {
                 length = try buffer.readUInt8()
-                let byte: UInt8 = try buffer.readUInt8()
+                let byte = try buffer.readUInt8()
                 randomAccessIndicator = (byte & 0x40) == 0x40
-                splicingPointFlag = (byte & 0x04) == 0x04
-                if splicingPointFlag {
-                    spliceCountdown = try buffer.readUInt8()
-                }
                 stuffingBytes = try buffer.readBytes(buffer.bytesAvailable)
             } catch {
                 logger.error("\(buffer)")
@@ -77,79 +64,6 @@ extension TSAdaptationField: DataConvertible {
 }
 
 extension TSAdaptationField: CustomDebugStringConvertible {
-    var debugDescription: String {
-        Mirror(reflecting: self).debugDescription
-    }
-}
-
-struct TSAdaptationExtensionField {
-    var length: UInt8 = 0
-    var legalTimeWindowFlag = false
-    var piecewiseRateFlag = false
-    var seamlessSpiceFlag = false
-    var legalTimeWindowOffset: UInt16 = 0
-    var piecewiseRate: UInt32 = 0
-    var spliceType: UInt8 = 0
-    var DTSNextAccessUnit = Data(count: 5)
-
-    init?(data: Data) {
-        self.data = data
-    }
-}
-
-extension TSAdaptationExtensionField: DataConvertible {
-    var data: Data {
-        get {
-            let buffer = ByteArray()
-                .writeUInt8(length)
-                .writeUInt8(
-                    (legalTimeWindowFlag ? 0x80 : 0) |
-                        (piecewiseRateFlag ? 0x40 : 0) |
-                        (seamlessSpiceFlag ? 0x1F : 0)
-                )
-            if legalTimeWindowFlag {
-                buffer.writeUInt16((legalTimeWindowFlag ? 0x8000 : 0) | legalTimeWindowOffset)
-            }
-            if piecewiseRateFlag {
-                buffer.writeUInt24(piecewiseRate)
-            }
-            if seamlessSpiceFlag {
-                buffer
-                    .writeUInt8(spliceType)
-                    .writeUInt8(spliceType << 4 | DTSNextAccessUnit[0])
-                    .writeBytes(DTSNextAccessUnit.subdata(in: 1 ..< DTSNextAccessUnit.count))
-            }
-            return buffer.data
-        }
-        set {
-            let buffer = ByteArray(data: newValue)
-            do {
-                var byte: UInt8 = 0
-                length = try buffer.readUInt8()
-                byte = try buffer.readUInt8()
-                legalTimeWindowFlag = (byte & 0x80) == 0x80
-                piecewiseRateFlag = (byte & 0x40) == 0x40
-                seamlessSpiceFlag = (byte & 0x1F) == 0x1F
-                if legalTimeWindowFlag {
-                    legalTimeWindowOffset = try buffer.readUInt16()
-                    legalTimeWindowFlag = (legalTimeWindowOffset & 0x8000) == 0x8000
-                }
-                if piecewiseRateFlag {
-                    piecewiseRate = try buffer.readUInt24()
-                }
-                if seamlessSpiceFlag {
-                    DTSNextAccessUnit = try buffer.readBytes(DTSNextAccessUnit.count)
-                    spliceType = DTSNextAccessUnit[0] & 0xF0 >> 4
-                    DTSNextAccessUnit[0] = DTSNextAccessUnit[0] & 0x0F
-                }
-            } catch {
-                logger.error("\(buffer)")
-            }
-        }
-    }
-}
-
-extension TSAdaptationExtensionField: CustomDebugStringConvertible {
     var debugDescription: String {
         Mirror(reflecting: self).debugDescription
     }
